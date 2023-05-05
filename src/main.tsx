86506bfd144b1f66345c4cc1984966e0b3a499bd
Adrià Ribas <ribasadria00@gmail.com>
2023-05-05 12:29:06 +0200

    Customizing Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(/* {
+  // Configuration examples (default values are a good approach)
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, // 5min
+      staleTime: 10 * 1000, // 10s
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false
+    }
+  }
+} */);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
