ac0b54ccbc1b5d597b94f89ef0f946022ba62b95
Adrià Ribas <ribasadria00@gmail.com>
2023-05-09 15:23:51 +0200

    Creating Zustand stores for managing state

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,16 @@
 import LoginStatus from './auth/LoginStatus';
+import { useCounterStore } from './counter';
 import { useTasks } from './tasks';
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  // Només es torna a renderitzar si canvia el counter
+  const counter = useCounterStore(s => s.counter);
 
   return (
     <nav className='navbar d-flex justify-content-between'>
       <span className='badge text-bg-secondary'>{tasks.length}</span>
+      <span className='badge text-bg-secondary'>{counter}</span>
       <LoginStatus />
     </nav>
   );
---
+++ src/state-management/auth/store.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+interface AuthStore {
+  user: string;
+  logIn: (username: string) => void;
+  logOut: () => void;
+}
+
+const useAuthStore = create<AuthStore>(set => ({
+  user: '',
+  logIn: username => set(() => ({ user: username })),
+  logOut: () => set(() => ({ user: '' }))
+}));
+
+export default useAuthStore;
